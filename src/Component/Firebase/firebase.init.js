294c9eec0b4c4d5a38f3e843323e087d1b81fa3c
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-04-03 15:30:13 +0600

    firebase :bulb:

--- src/Component/Firebase/firebase.init.js
+++ src/Component/Firebase/firebase.init.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 
  // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+// firebase.init
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
