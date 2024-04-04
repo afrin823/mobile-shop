00332c8d58ffd13d0dc0e6cac33cbf8bcdebfbf3
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-04-04 09:56:39 +0600

    :whale: Hook

--- src/Component/Hook/useUser.js
+++ src/Component/Hook/useUser.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import auth from '../Firebase/firebase.init'
 
+// Hook
 const useUser = () => {
   const [user] = useAuthState(auth)
   const [currentUser, setUser] = useState({})
