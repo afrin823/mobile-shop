44df20bdcd40aecbaaadafe3151223b1c6c71919
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-04-04 10:00:45 +0600

    :checkered_flag: Product order

--- src/Component/Product/Order.js
+++ src/Component/Product/Order.js
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 import auth from '../Firebase/firebase.init';
 import Loading from '../Loading/Loading';
-
+// Product
 const Order = ({ product, show, setShow }) => {
     const [error, setError] = useState('')
     const [user, userLoading] = useAuthState(auth)
