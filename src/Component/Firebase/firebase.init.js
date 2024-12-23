a7dbc2452a5ef0bf3f42dfba905cd296e0659a99
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-23 11:49:56 +0600

    Firebase init 🫐

--- src/Component/Firebase/firebase.init.js
+++ src/Component/Firebase/firebase.init.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 // firebase.init
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+// Firebase init
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
